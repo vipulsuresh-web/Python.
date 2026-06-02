6c4f877956cc9fdfa3847b2d16f8dae2fc25af23
vipulsuresh-web <vipulsuresh0901@gmail.com>
2026-06-02 16:33:13 +0530

    Add files via upload

---
+++ vipul lab 2.py
@@ -0,0 +1,119 @@
+#lab task 2
+#1)
+A=int(input("Enter a number:"))
+if A%2==0:
+    print("Even")
+else:
+    print("Odd")
+#2)
+A=int(input("Enter age:"))
+if A>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
+
+
+#3)
+A=int(input("Enter mark:"))
+if A>=40:
+    print("pass")
+else:
+    print("Fail")
+#4)
+A=int(input("Enter number:"))
+if A>0:
+    print("positive")
+elif A==0:
+    print("neither positive nor negative")
+else:
+    print("negative")
+#5)
+A=int(input("Enter mark:"))
+if A>=90 and A<=100:
+    print("A")
+elif A>=75 and A<=89:
+    print("B")
+elif A>=50 and A<=74:
+    print("C")
+else:
+    print("Fail")
+#6)
+A=int(input("Enter first number:"))
+B=int(input("Enter second number:"))
+C=int(input("Enter third number:"))
+if A>=B and A>=C:
+    print("A is greater")
+elif B>=A and B>=C:
+    print("B is greater")
+else:
+    print("C is greater")
+#7)
+A=int(input("Enter a num:"))
+if A==1:
+    print("Monday")
+elif A==2:
+    print("tuesday")
+elif A==3:
+    print("wednesday")
+elif A==4:
+    print("thursday")
+elif A==5:
+    print("friday")
+elif A==6:
+    print("saturday")
+elif A==7:
+    print("sunday")
+else:
+    print("correct num podunga")
+#9)
+A=str(input("Enter username:"))
+B=str(input("Enter password:"))
+if A=="nagul_45":
+    if B=="N0904":
+        print("login successfull")
+    else:
+        print("wrong password")
+else:
+    print("Invalid username")
+
+#10)
+A=str(input("Enter Signal Colour:"))
+if A=="Red":
+    print("stop")
+elif A=="Yellow":
+    print("Get ready")
+elif A=="Green":
+    print("Go")
+else:
+    print("Error")
+
+
+
+    
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
